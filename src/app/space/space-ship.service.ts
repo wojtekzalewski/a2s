611c2371e177f9b47f24d01ae087981c7721ae82
wojtekzalewski <tomaszwojtekzalewski@gmail.com>
2025-05-01 20:09:47 +0200

    implementacja uslugi do budowy statkow

---
+++ src/app/space/space-ship.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { interval, map, Observable, take} from 'rxjs';
+import { OrderFormValue } from './order-form-value';
+import { BomberShip, FighterShip, SpaceShip } from './space-ship';
+import { SpaceShipType } from './space-ship-type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpaceShipService {
+  static shipProductionTime = 2000;
+
+  produceShips({shipCount, shipType}): OrderFormValue): Obesrvable<spaceShip> {
+    return interval(SpaceShipService.shipProductionTime).pipe(
+      map(() => shipType === SpaceShipType.Fighter ? new FighterShip() : new BomberShip()),
+      take(shipCount)
+    );
+  }
+
+  constructor() { }
+}
