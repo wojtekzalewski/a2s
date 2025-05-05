9d94efd0e7ac37f1e06498b66b95df4352c4c0f2
wojtekzalewski <tomaszwojtekzalewski@gmail.com>
2025-05-05 16:14:56 +0200

    kolekcja statkow

--- src/app/space/space-ship.service.ts
+++ src/app/space/space-ship.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { interval, map, Observable, take} from 'rxjs';
+import { BehaviorSubject, interval, map, Observable, take, tap} from 'rxjs';
 import { OrderFormValue } from './order-form-value';
 import { BomberShip, FighterShip, SpaceShip } from './space-ship';
 import { SpaceShipType } from './space-ship-type';
@@ -10,11 +10,22 @@ import { SpaceShipType } from './space-ship-type';
 export class SpaceShipService {
   static shipProductionTime = 2000;
 
+  hangarShips = new BehaviorSubject<SpaceShip[]>([]);
+
+  hangarShipCount = this.hangarShips.pipe(
+    map((ships) => ships.length)
+  );
+
   produceShips({shipCount, shipType}: OrderFormValue): Observable<SpaceShip> {
     return interval(SpaceShipService.shipProductionTime).pipe(
       map(() => shipType === SpaceShipType.Fighter ? new FighterShip() : new BomberShip()),
-      take(shipCount)
-    );
+      take(shipCount),
+      tap((spaceShip) =>
+      this.hangarShips.next([
+        ...this.hangarShips.getValue(),
+      ])
+      )
+    )
   }
 
   constructor() { }
