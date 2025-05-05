27a0a090daa62b4a82dd74ba48159144c21913ae
wojtekzalewski <tomaszwojtekzalewski@gmail.com>
2025-05-05 20:15:13 +0200

    katalog space

---
+++ src/app/space/pilot.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Pilot, PilotAttrs } from './pilot';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PilotService {
+
+  private http = inject(HttpClient);
+
+  getPilots(): Observable<Pilot[]> {
+    return this.http.get<PilotAttrs[]>(`${environment.apiUrl}/pilots`).pipe(
+      map((data) => data.map((pilotAttrs) => new Pilot(pilotAttrs)))
+    );
+  }
+
+  constructor() { }
+}
