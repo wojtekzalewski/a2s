dfbfd691dfac849274af66b3ce29548f6153c529
wojtekzalewski <tomaszwojtekzalewski@gmail.com>
2025-05-07 15:43:38 +0200

    testy dla produkcji statkow

---
+++ cypress/e2e/ship-production.cy.ts
@@ -0,0 +1,10 @@
+describe('ShipProduction', () => {
+  it('', () => {
+    cy.visit('/');
+    cy.get('input[type=radio]').first().click();
+    cy.get('input[type=number]').clear().type('2');
+    cy.get('form button').click();
+    cy.wait(4000);
+    cy.get('app-space-ship').should('have.length', 2);
+  });
+});
